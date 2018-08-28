1781aded52d6130f677fb8054677d2e438857a53
skarimo <karimovj@gmail.com>
2018-08-28 11:05:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get '/' do
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/new' do
+    @details = params
+    erb :display_puppy
+  end
+
+
 end
